f389c896e28498ac43f4fc11b4094012a9aead06
pdiakumis <peterdiakumis@gmail.com>
2026-02-11 01:22:17 +1100

    standardise box imports

--- _setup.R
+++ _setup.R
@@ -1,3 +1,11 @@
-use("dplyr")
-use("tibble", "tibble")
+{
+  suppressPackageStartupMessages(use("dplyr"))
+  use("tibble", "tibble")
+}
 options(box.path = here::here("box"))
+box::use(
+  tw_utils = wigits/utils,
+  tw_plot = wigits/plot,
+  tw_table = wigits/table,
+)
+dat <- tw_utils$get_data()
